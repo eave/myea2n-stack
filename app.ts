191b1b5c2a0989bd368de396b86054be2ac302aa
Mario Ponticello <marioPonticello@dockerMac.local>
2015-05-10 00:51:46 -0400

    Build out index.html and app.ts

--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
 import {Component, View, bootstrap} from 'angular2/angular2';
 
+// The Component annotation defines the <my-app></my-app> HTML tag
 @Component({
   selector: 'my-app'
 })
 
+// The View annotation defines the inline template for the component
 @View({
   template: '<h1>Hello {{ name }}</h1>'
 })
+
 // Component controller
 class MyAppComponent {
   name: string;
@@ -16,3 +19,5 @@ class MyAppComponent {
     this.name = 'Alice';
   }
 }
+
+bootstrap(MyAppComponent);
