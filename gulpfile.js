40a8434a7e05f38ebd2518d42bbdc780d6b84b45
Mario Ponticello <marioPonticello@dockerMac.local>
2015-05-13 23:44:39 -0700

    Set up serve task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('serve', function(done) {
+  browserSync({
+    // open: false,
+    port: 9000,
+    server: {
+      baseDir: ['client']
+    }
+  });
+
+  gulp.watch(['client/**/*'],reload);
+});
+
+gulp.task('default', ['serve']);
